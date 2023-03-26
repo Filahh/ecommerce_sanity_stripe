c0a784da7f7b8f56896b6fed6ef651c4acd7fdab
Henry <henryvictor215@gmail.com>
2023-03-26 23:58:27 +0100

    final commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JsMastery Store</title>
+        <title>Fila Store</title>
       </Head>
       <header>
         <Navbar />
